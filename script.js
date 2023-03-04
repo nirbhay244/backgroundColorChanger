2d59940db0833a9d87d05df8ce00110029e08e7a
nirbhay244 <99859216+nirbhay244@users.noreply.github.com>
2023-03-04 14:14:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const change = () => {
+    document.body.style.backgroundColor = randomColor();
+}
+
+const randomColor = () => {
+    let val = "0123456789ABCDEF";
+    let cons = "#";
+
+    for(let i = 0; i < 6; i++){
+        cons = cons + val[Math.floor(Math.random() * 16)]
+    }
+    return cons;
+} 
